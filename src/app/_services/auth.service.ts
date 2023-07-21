08b827f67fee7637c2301307e7199adc5bb3da78
lotfi0310 <lotfilouiz@gmail.com>
2023-07-21 17:50:23 +0200

    get client profile-image when authentificate and save it to storage session

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, map, of, throwError } from 'rxjs';
 
 const AUTH_API = 'http://localhost:9090/api/auth/';
 
@@ -15,7 +15,8 @@ export class AuthService {
   AUTH_API = 'http://localhost:9090/api/auth/';
   Verif_API='http://localhost:9090/api/auth/verifyUserByusername'; 
   Validate_OTP='http://localhost:9090/api/auth/ValidateAccount';
-  private apiUrl = 'http://example.com/api'; // Replace this with your server's API URL
+  apiUrl = 'http://localhost:9090/api/auth/user/profile-picture'; // Replace this with your server's API URL
+ 
 
   constructor(private http: HttpClient) { }
   
@@ -51,6 +52,20 @@ export class AuthService {
   }
   logout(): Observable<any> {
     return this.http.post(AUTH_API + 'signout', { }, httpOptions);
+  }
+  getImage(token: string,imageName: string): Observable<any>{
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer' + token,
+      'Content-Type': 'image/*', // Set the appropriate content type for your image
+      'Accept': 'image/*' // Set the appropriate accept type for your image
+    });
+    return this.http.get(`${this.apiUrl}/${imageName}`, { responseType: 'blob' })
+    .pipe(
+      catchError((error) => {
+        console.error('Error fetching profile picture:', error);
+        return throwError('Unable to fetch profile picture');
+      })
+    );  
   }
 
   updateUser(updatedUser: any, password: string) {
--- src/app/_services/storage.service.ts
+++ src/app/_services/storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 const USER_KEY = 'auth-user';
 const USER_PICTURE_KEY = 'user-picture'; // Key for storing the user's picture URL
@@ -8,36 +9,84 @@ const USER_PICTURE_KEY = 'user-picture'; // Key for storing the user's picture U
 })
 export class StorageService {
 
-  constructor() {}
+  constructor(private authService:AuthService) {}
 
   clean(): void {
     window.sessionStorage.clear();
   }
 
-  public saveUser(user: any, pictureUrl?: string): void {
+  public saveUser(user: any, pictureBlob?: Blob|null, callback?: () => void): void {
     window.sessionStorage.removeItem(USER_KEY);
     window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
-    
-    if (pictureUrl) {
-      window.sessionStorage.setItem(USER_PICTURE_KEY, pictureUrl); // Save the picture URL if provided
+  
+    if (pictureBlob) {
+      // Convert the Blob data to a data URL and save it in the session storage
+      this.blobToDataURL(pictureBlob, (dataURL) => {
+        window.sessionStorage.setItem(USER_PICTURE_KEY, dataURL);
+        if (callback) {
+          callback(); // Call the provided callback once the picture data is saved
+        }
+      });
+    } else {
+      if (callback) {
+        callback(); // Call the provided callback if no picture data is provided
+      }
     }
   }
+  
+
+
+  private blobToDataURL(blob: Blob, callback: (dataURL: string) => void) {
+    const reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result as string);
+    };
+    reader.readAsDataURL(blob);
+  }
+
 
   public getUserPicture(): string | null {
     return window.sessionStorage.getItem(USER_PICTURE_KEY);
   }
 
+
   public getUser(): any {
     const user = window.sessionStorage.getItem(USER_KEY);
-    if (user) {
+    const picture = window.sessionStorage.getItem(USER_PICTURE_KEY);
+  
+    if (user && picture) {
+      return JSON.parse(user);
+
+    } else if (user && !picture) {
+      // User data is available, but picture data is missing.
+      // Fetch the picture using the access token and save it in the session storage.
+      const token = JSON.parse(user)['accessToken'];
+      const processedToken = token.replace('achref=', '').split(';')[0];
+      const username=JSON.parse(user)['profile'].username ;
+       this.authService.getImage(processedToken, `${username}.png`).subscribe(
+        (res) => {
+            this.saveUser(user, res);
+            console.log(res);
+          
+        },
+        (err) => {
+          console.log('error', err);
+          
+        }
+      );
+  
+      // Return the user data immediately without waiting for the picture data.
       return JSON.parse(user);
     }
-
-    return {};
+  
+    // Return null if user data is not available.
+    return null;
   }
+  
 
   public isLoggedIn(): boolean {
     const user = window.sessionStorage.getItem(USER_KEY);
+      
     if (user) {
       return true;
     }
