68dccb31e0da10e3f0e563c4f30aa6f4289a9e69
lotfi0310 <lotfilouiz@gmail.com>
2023-07-28 17:55:18 +0200

    fix bugs

--- src/app/client/login/login.component.ts
+++ src/app/client/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/_services/auth.service';
 import { StorageService } from 'src/app/_services/storage.service';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -19,6 +20,7 @@ export class LoginComponent {
   isLoginFailed = false;
   errorMessage = '';
   roles: string[] = [];
+  isAdmin = false;
 
   constructor(
     private authService: AuthService,
@@ -37,15 +39,37 @@ export class LoginComponent {
       next: data => {
         this.isVerified = data.verified;
         console.log(this.isVerified);
-
+          
         if (this.isVerified) {
+
           this.authService.login(username, password).subscribe({
             next: data => {
               this.storageService.saveUser(data);
               this.isLoginFailed = false;
               this.isLoggedIn = true;
               this.roles = this.storageService.getUser().roles;
-              this.router.navigate(['/client/home']);
+              this.roles.map((i)=>{
+                if (i === 'ROLE_ADMIN') {
+                  this.isAdmin = true;
+                 
+                  setTimeout(() => {
+                    this.authService.logout();
+                    sessionStorage.clear();
+                    this.isLoginFailed = true;
+                    this.isLoggedIn = false;
+                    this.router.navigate(['/admin/login']);
+                  }, 3000);
+                }
+                // For non-admin users
+                else if (i != 'ROLE_ADMIN')  {
+                  this.isAdmin=false
+                  this.isLoginFailed = false;
+                  this.isLoggedIn = true;
+                  this.router.navigate(['/client/home']);
+                  // No need to use setTimeout here for non-admin users.
+                }
+              })
+
             },
             error: err => {
               this.errorMessage = err.error.message;
